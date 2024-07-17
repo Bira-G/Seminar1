0dcaed23e8ce775f2242d4ba733179699ef6b939
Ilyas Zhangaziev <bira-g@mail.ru>
2024-07-17 20:29:38 +0500

    add Seminar1:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int firstNumber = 7;
+int secondNumber = 3;
+
+if (firstNumber == secondNumber * secondNumber)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+    System.Console.WriteLine("No");
+}
